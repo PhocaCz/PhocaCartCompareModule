8ad72fc48da0305f2e576fbde39903256b0a0fad
Jan <info@phoca.cz>
2019-02-10 15:14:40 +0100

    3.1.7

--- mod_phocacart_compare.php
+++ mod_phocacart_compare.php
@@ -7,25 +7,35 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
  
-defined('_JEXEC') or die('Restricted access');// no direct access
+defined('_JEXEC') or die;// no direct access
 
 if (!JComponentHelper::isEnabled('com_phocacart', true)) {
 	$app = JFactory::getApplication();
 	$app->enqueueMessage(JText::_('Phoca Cart Error'), JText::_('Phoca Cart is not installed on your system'), 'error');
 	return;
 }
-if (! class_exists('PhocaCartLoader')) {
+
+JLoader::registerPrefix('Phocacart', JPATH_ADMINISTRATOR . '/components/com_phocacart/libraries/phocacart');
+/*
+if (! class_exists('PhocacartLoader')) {
     require_once( JPATH_ADMINISTRATOR.'/components/com_phocacart/libraries/loader.php');
 }
+phocacartimport('phocacart.path.route');
 phocacartimport('phocacart.render.renderjs');
 phocacartimport('phocacart.compare.compare');
-phocacartimport('phocacart.category.categorymultiple');
-PhocaCartRenderJs::renderAjaxRemoveFromCompare();
+phocacartimport('phocacart.category.categorymultiple');*/
 
-$compare	= new PhocaCartCompare();
-echo '<div id="phItemCompareBox">';
-echo $compare->renderList();
-echo '</div>';
-echo '<div id="phContainerModuleCompare"></div>';
+$lang = JFactory::getLanguage();
+//$lang->load('com_phocacart.sys');
+$lang->load('com_phocacart');
+
+$p['module_description']	= $params->get( 'module_description', '' );
+$moduleclass_sfx 			= htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8');
+
+JHTML::stylesheet('media/com_phocacart/css/main.css' );
+
+PhocacartRenderJs::renderAjaxRemoveFromCompare();
+
+$compare	= new PhocacartCompare();
 require(JModuleHelper::getLayoutPath('mod_phocacart_compare'));
 ?>
--- tmpl/default.php
+++ tmpl/default.php
@@ -6,7 +6,20 @@
  * @copyright Copyright (C) Jan Pavelka www.phoca.cz
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
+
+if ($p['module_description'] != '') {
+	echo '<div class="ph-mod-desc">'.$p['module_description'].'</div>';
+}
+echo '<div id="phItemCompareBox" class="ph-compare-module-box'.$moduleclass_sfx .' phItemCompareBox">';
+echo $compare->renderList();
+echo '</div>';
+echo '<div id="phContainerModuleCompare"></div>';
+
+// Get count of items, count can be displayed e.g. in case the list is hidden
+// Add it to DIV ID because it will be changed per AJAX
+// $count = $compare->getComapareCountItems();
+// echo '<div class="phItemCompareBoxCount" id="phItemCompareBoxCount">'. $compare->getComapareCountItems().'</div>';
 ?>
 
 
